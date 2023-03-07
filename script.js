a51f3171496090e514037212e243f6fca04cdbc4
Kristian <bollepusdodgebolle@gmail.com>
2023-03-07 23:27:30 +0100

    new functions nd stuff
    laget functions som bruker addDoc function fra firebase library osv
    . fikset layout og deisgn av knapper. Funker

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 
-    console.log("hello world");
+    console.log("Fuck you world");
 
- 
+  
+  
 
-      // const wievAct() {
-      //   wievAct.addEventListener(
-             
-      //   getDoc(db, activities,activities)
-      //   )
-      // }
+  function ViewAll() {
+    const dinput = document.getElementById("viewActBtn").value;
+    console.log("funky town");
+    document.getElementById("viewAct").innerHTML =  "These are the activities ongoing"
+    // document.getElementById("myImage").src = "volleyball.jpg";
+  }
+
+  // document.getElementById("ViewAct").addEventListener("click", ViewAll)
+  // {
+  //   console.log("pressed, run ViewAll");
+  // }
+
+
+
+  // display all activities inni activites tab i firestore tingen
+
+
+
